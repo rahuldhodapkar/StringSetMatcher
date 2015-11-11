3f7b21ac07dca35dad9c320c022c122208e07167
rahuldhodapkar <rahul.m.dhodapkar@gmail.com>
2015-11-11 16:24:10 -0500

    changed gitignore to exclude distribution artifacts

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(
   packages = ['string_set_matcher'],
   version = '0.1.0',
   description = 'determine a best fuzzy match between sets of strings',
+  long_description = 'determine a best fuzzy match between sets of strings',
   author = 'Rahul Dhodapkar',
   author_email ='rahul.m.dhodapkar@gmail.com',
   url = 'https://github.com/rahuldhodapkar/StringSetMatcher',
   download_url = 'https://github.com/rahuldhodapkar/StringSetMatcher/tarball/0.1',
   keywords = ['set', 'string', 'fuzzy', 'matching'],
+  license = 'MIT',
   classifiers = [],
 )
