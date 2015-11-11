b4e7a89d677607c04b0424ccfeb4165ea03c95a7
rahuldhodapkar <rahul.m.dhodapkar@gmail.com>
2015-11-11 14:47:17 -0500

    added support for different dimensional lists

--- match_string_sets.py
+++ match_string_sets.py
@@ -6,6 +6,10 @@ from fuzzywuzzy import fuzz
 import itertools
 
 def gen_elem_scores(f,o):
+    if not isinstance(f, basestring) and f is not None \
+    or not isinstance(o, basestring) and o is not None:
+        raise TypeError('lists contain values other than strings')
+
     if f is not None and o is not None:
         return fuzz.ratio(f,o)
     else:
@@ -25,14 +29,27 @@ def score_set_match(frm, onto):
 def map_string_sets(frm, onto):
     mapped_scores = []
 
+    orig_frm_len = len(frm)
+
+    max_length = max(len(frm), len(onto))
+    pad_frm = max_length - len(frm)
+    pad_onto = max_length - len(onto)
+
+    if pad_frm > 0:
+        frm.extend([None] * pad_frm)
+
+    if pad_onto > 0:
+        onto.extend([None] * pad_onto)
+
     perm_gen = itertools.permutations(frm)
     perm_idx = itertools.permutations(range(len(frm)))
+
     for perm, ix in itertools.izip(perm_gen, perm_idx):
         ix_mapping = map(lambda x: ix.index(x), range(len(ix)))
-        mapped_scores.append( (ix_mapping, score_set_match(perm, onto) ) )
+        mapped_scores.append( (ix_mapping[:orig_frm_len] , score_set_match(perm, onto) ) )
 
     mapped_scores.sort(key=lambda x: -x[1])
     return mapped_scores
 
-print map_string_sets(['name', 'change', 'depth'], ['full name', 'change type', 'deepness'])
+print map_string_sets(['name', 'change'], ['full name', 'change type', 'deepness'])
 
