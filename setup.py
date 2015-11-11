c6fe80468c1b0a6b18a98ab0f4e5355c65a944d0
rahuldhodapkar <rahul.m.dhodapkar@gmail.com>
2015-11-11 15:18:40 -0500

    getting package ready for release

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'string_set_matcher',
+  packages = ['string_set_matcher'],
+  version = '0.1',
+  description = 'determine a best fuzzy match between sets of strings',
+  author = 'Rahul Dhodapkar',
+  author_email ='rahul.m.dhodapkar@gmail.com',
+  url = 'https://github.com/rahuldhodapkar/StringSetMatcher',
+  download_url = 'https://github.com/rahuldhodapkar/StringSetMatcher/tarball/0.1',
+  keywords = ['set', 'string', 'fuzzy', 'matching'],
+  classifiers = [],
+)
